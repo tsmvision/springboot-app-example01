55233358a139dc670a26baf25bc2e43cb3070cf2
lukelee <lukelee01@gmail.com>
2021-02-12 21:35:26 -0500

    remove unused import from MemberServiceTest

--- backend/src/test/java/com/example/backend/service/MemberServiceTest.java
+++ backend/src/test/java/com/example/backend/service/MemberServiceTest.java
@@ -2,18 +2,13 @@ package com.example.backend.service;
 
 import com.example.backend.domain.Member;
 import com.example.backend.repository.MemberRepository;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
-
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
-
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
-import static org.junit.jupiter.api.Assertions.*;
+
 
 @SpringBootTest
 @Transactional
