adea974d10c3f03bb5b61ed483c7a5e27c375435
lukelee <lukelee01@gmail.com>
2021-02-13 22:16:11 -0500

    add /api/ in the rest controllers in the backend

--- backend/src/main/java/com/example/backend/controller/HomeController.java
+++ backend/src/main/java/com/example/backend/controller/HomeController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
 @RestController
-@RequestMapping("/")
+@RequestMapping("/api")
 public class HomeController {
 
     @GetMapping
--- backend/src/main/java/com/example/backend/controller/MemberController.java
+++ backend/src/main/java/com/example/backend/controller/MemberController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
 @RestController
-@RequestMapping("/member")
+@RequestMapping("/api/member")
 @RequiredArgsConstructor
 public class MemberController {
 
